d79b860d2bd4490b60439b51c4f9efc5338df41d
Ossian Laurinharju <ossian.laurinharju@gmail.com>
2014-04-27 13:36:48 +0300

    viikko6 2

--- Laskin/src/ohtu/komennot/Laskuoperaatio.java
+++ Laskin/src/ohtu/komennot/Laskuoperaatio.java
@@ -16,6 +16,7 @@ public abstract class Laskuoperaatio implements Komento{
 	private Sovelluslogiikka sovellus;
 	private JTextField tuloskenttä;
 	private JTextField syötekenttä;
+	protected int edellinenArvo;
 	
 	public Laskuoperaatio(Sovelluslogiikka sovellus, JTextField tuloskenttä, JTextField syötekenttä) {
 		this.sovellus = sovellus;
@@ -25,6 +26,7 @@ public abstract class Laskuoperaatio implements Komento{
 	
 	@Override
 	public void suorita() {
+		edellinenArvo = sovellus.tulos();
 		int arvo = 0;
         try {
             arvo = Integer.parseInt(syötekenttä.getText());
@@ -33,13 +35,18 @@ public abstract class Laskuoperaatio implements Komento{
 		int tulos = laske(sovellus.tulos(), arvo); 
 		sovellus.setTulos(tulos);
 		
-		syötekenttä.setText("");
-        tuloskenttä.setText("" + sovellus.tulos());
+		päivitäKentät();
 	}
 	
 	@Override
 	public void peru() {
-		
+		sovellus.setTulos(edellinenArvo);
+		päivitäKentät();
+	}
+	
+	private void päivitäKentät() {
+		syötekenttä.setText("");
+        tuloskenttä.setText("" + sovellus.tulos());
 	}
 	
 	protected abstract int laske(int a, int b);
